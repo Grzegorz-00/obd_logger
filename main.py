c6f899a5a6307e68706e8355f4094c217fa37e87
grzegorz-00 <grzegorz00new@gmail.com>
2021-07-12 21:36:04 +0200

    Add FUELPW1 pids

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import obd
+from obd import OBDCommand, Unit
+from obd.protocols import ECU
+from obd.utils import bytes_to_int
 import argparse
 import time
 from datetime import datetime
+
+def fuelpw(messages):
+    """ decoder for RPM messages """
+    d = messages[0].data # only operate on a single message
+    d = d[2:] # chop off mode and PID bytes
+    v = d[0]*2.734 + d[1]*0.0106  # helper function for converting byte arrays to ints
+    return v * Unit.RPM # construct a Pint Quantity
 
 class ObdLogger:
     def __init__(self, filename, refresh_rate_ms):
@@ -17,12 +27,11 @@ class ObdLogger:
         self._commands['INTAKE_PRESSURE'] = obd.commands.INTAKE_PRESSURE
         self._commands['FUEL_INJECT_TIMING'] = obd.commands.FUEL_INJECT_TIMING
         self._commands['LONG_FUEL_TRIM_1'] = obd.commands.LONG_FUEL_TRIM_1
-        self._commands['LONG_O2_TRIM_B1'] = obd.commands.LONG_O2_TRIM_B1
         self._commands['SHORT_FUEL_TRIM_1'] = obd.commands.SHORT_FUEL_TRIM_1
-        self._commands['SHORT_O2_TRIM_B1'] = obd.commands.SHORT_O2_TRIM_B1
+        self._commands['FUELPW1'] = OBDCommand("FUELPW1", "Fuel injector pulse width", b"1141",  2, fuelpw)
 
         self._connection = obd.OBD()
-
+        self._connection.supported_commands.add(self._commands['FUELPW1'])
         if self._connection.status() != obd.OBDStatus.CAR_CONNECTED:
             raise Exception("No connection")
 
